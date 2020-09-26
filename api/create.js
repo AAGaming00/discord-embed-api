7ac3c7fb585b2ec5cdc2e0ca6ce0ca07bd7f5aa5
AAGaming <aa@bigdumb.gq>
2020-09-26 19:38:18 -0400

    console log b a d

--- api/create.js
+++ api/create.js
@@ -57,7 +57,7 @@ var url = decodeURIComponent(request.url);
 
   var requestData = "";
   requestData = request.body;
-  console.log(requestData)
+  console.log(JSON.parse(JSON.stringify(requestData))
     if (requestData != "") {
       try {
         var embedID = randomString(10);
