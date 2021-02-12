85a7eddaa39da9a4d07b8e9600098e2a2ce820e2
AAGaming <aa@bigdumb.gq>
2021-02-12 10:17:18 -0500

    oops

--- api/index.js
+++ api/index.js
@@ -30,7 +30,7 @@ function escapeHtml(unsafe) {
 
 module.exports = async (request, response) => {
       const url = request.url
-      var embedID = request.query.id
+      var embedID = request.query.id.replace('.json', '')
       if (request.headers["user-agent"].includes('Discordbot')) {
       console.log("User requested an embed: " + embedID);
 
